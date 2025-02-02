a3934fd25ac088b9997ecc1f882fedbb1c3cd342
4OH4Loop <semperchuck@gmail.com>
2025-02-02 14:32:24 -0500

    Ch 1 Fundamentals Pt 1

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -8,16 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.red)
+                .frame(minHeight: 150)
+                .frame(maxWidth: .infinity)
+                .border(.orange, width: 2)
+                .padding()
+            
+            HStack {
+                Button("Awesome") {
+                    let messageString = ["You Are Awesome!", "You Are Great!"]
+                    message = messageString[0]
+                }
+                Button("Great!") {
+                    let messageString = ["You Are Awesome!", "You Are Great!"]
+                    message = messageString[1]
+                }
+
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .border(.orange, width: 5)
+            }
+            
         }
-        .padding()
     }
-}
 
 #Preview {
     ContentView()

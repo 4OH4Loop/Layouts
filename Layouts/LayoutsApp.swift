98293bd4c0115856c017984d34075fb33cb1f786
4OH4Loop <semperchuck@gmail.com>
2025-02-02 13:23:50 -0500

    Initial Commit

---
+++ Layouts/LayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsApp.swift
+//  Layouts
+//
+//  Created by Carolyn Ballinger on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

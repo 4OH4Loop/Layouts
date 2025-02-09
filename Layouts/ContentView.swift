419137ebfc124ddf02263cd1a00f70129eb0c9e9
4OH4Loop <semperchuck@gmail.com>
2025-02-09 13:58:52 -0500

    Ch 1 Fundamentals Layout Pt 3

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -10,56 +10,55 @@ import SwiftUI
 struct ContentView: View {
     @State private var messageString = "When the Genius Bar Needs Help, They Call You"
     var body: some View {
-
-            
-            VStack {
-                Text("You Have Skills!")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .foregroundStyle(.goldBC)
-                    .padding()
-                    .background(Color("maroon - BC"))
-                    .clipShape(RoundedRectangle(cornerRadius: 15))
-                
-                Spacer()
-                
-                Text(messageString)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundStyle(.red)
-                    .frame(minHeight: 150)
-                    .frame(maxWidth: .infinity)
-                //                .border(.orange, width: 2)
-                    .padding()
-                
-                Spacer()
-                
-                HStack {
-                    Button("Awesome") {
-                        messageString = "You Are Awesome!"
-                    }
-                    
-                    Spacer()
-                    
-                    Button("Great!") {
-                        messageString = "You Are Great!"
-                    }
-                    
-                }
-                .buttonStyle(.borderedProminent)
-                .font(.title2)
-                //            .border(.orange, width: 5)
+        
+        VStack {
+           Text("Awesome!")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.white)
                 .padding()
-                .tint(.vermillion)
-            }
-            .background(
-                Gradient(colors: [.white, .blue, .red])
-            )
-        }
+                .frame(maxWidth: .infinity)
+                .background(.indigo)
+                
             
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.red)
+                .frame(minHeight: 150)
+                .frame(maxWidth: .infinity)
+                .padding()
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome") {
+                    messageString = "You Are Awesome!"
+                }
+                
+                Spacer()
+                
+                Button("Great!") {
+                    messageString = "You Are Great!"
+                }
+                
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .padding()
+            
+            Rectangle()
+                .frame(height: 0)
+                .background(.indigo)
+                
         }
+    }
+    
+}
 
 
 #Preview {
